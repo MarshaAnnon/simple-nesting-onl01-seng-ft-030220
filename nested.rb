f19be3a35989002edf3f659559bfa75bda7606d5
Marsha Annon <marshaannon@gmail.com>
2020-03-08 20:24:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,26 +70,28 @@ def adding_matz
 #    :languages => ["LISP", "C"]
 # }
 
-def adding_matz
-programmer_hash =
-			{
-			:yukihiro_matsumoto => {
-			  :known_for => "Ruby",
-			  :languages => ["LISP", "C"]
-			},
-			:grace_hopper => {
-				:known_for => "COBOL",
-				:languages => ["COBOL", "FORTRAN"]
-			},
-			:alan_kay => {
-				:known_for => "Object Orientation",
-				:languages => ["Smalltalk", "LISP"]
-			},
-			:dennis_ritchie => {
-				:known_for => "Unix",
-				:languages => ["C"]
-			}
-	 }
+	programmer_hash =
+ 		{
+        :grace_hopper => {
+          :known_for => "COBOL",
+          :languages => ["COBOL", "FORTRAN"]
+        },
+        :alan_kay => {
+          :known_for => "Object Orientation",
+          :languages => ["Smalltalk", "LISP"]
+        },
+        :dennis_ritchie => {
+          :known_for => "Unix",
+          :languages => ["C"]
+        }
+     }
+
+		 programmer_hash[:yukihiro_matsumoto] = {
+   :known_for => "Ruby",
+   :languages => ["LISP", "C"]
+ }
+
+
 end
 
 def changing_alan
