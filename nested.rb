3630a50787ac8a1635eac41a2b46c83c435b0493
Marsha Annon <marshaannon@gmail.com>
2020-03-08 19:19:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -40,7 +40,10 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+
+		 programmer_hash[:alan_kay][:known_for]
 end
+
 
 def dennis_ritchies_language
 	programmer_hash =
@@ -58,6 +61,8 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+
 end
 
 def adding_matz
