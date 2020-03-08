e6a28f53fa86f6a7385fc446c5c48638306864ec
Marsha Annon <marshaannon@gmail.com>
2020-03-08 18:49:39 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,7 +21,7 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	known_for = programmer_hash[:hopper][:known_for]
+	known_for = programmer_hash[:grace_hopper][:known_for]
 
 	programmer_hash =
  		{
